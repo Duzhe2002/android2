fd20aaf9dea72b81143a5a3b1c55556d39ab23cf
GaoChengquan <529390053@qq.com>
2021-11-18 15:17:01 +0800

    save

--- app/src/main/java/com/situ/helloworld/FirstActivity.java
+++ app/src/main/java/com/situ/helloworld/FirstActivity.java
@@ -10,7 +10,7 @@ import android.widget.Button;
 
 public class FirstActivity extends AppCompatActivity {
     private static final String TAG = "FirstActivity";
-    private Button button = null;
+    private Button button; // null
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +18,7 @@ public class FirstActivity extends AppCompatActivity {
         setContentView(R.layout.activity_first);
 
         button = (Button) findViewById(R.id.button);
+        // null.方法 或者 null.属性 就会报NullPointerException
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,8 +27,11 @@ public class FirstActivity extends AppCompatActivity {
                 Log.d(TAG, "onClick: ");
 
                 // 显式Intent
-                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
-                startActivity(intent);
+               Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                intent.putExtra("data", "HelloSecondActivity");
+                // startActivity(intent);
+                // requestCode
+                startActivityForResult(intent, 1);
 
                 /*隐式Intent*/
                 /*Intent intent = new Intent();
@@ -41,5 +45,19 @@ public class FirstActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    String dataReturn = data.getStringExtra("data_return");
+                    Log.d(TAG, "onActivityResult: " + dataReturn);
+                }
+                break;
+            case 2:
+                break;
+        }
     }
 }
--- app/src/main/java/com/situ/helloworld/SecondActivity.java
+++ app/src/main/java/com/situ/helloworld/SecondActivity.java
@@ -1,13 +1,36 @@
 package com.situ.helloworld;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
+    private static final String TAG = "SecondActivity";
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        String data = intent.getStringExtra("data");
+        Log.d(TAG, "onCreate: " + data);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("data_return", "HelloFirstActivity");
+                // resultCode
+                setResult(RESULT_OK, intent);
+                // 销毁Activity
+                finish();
+            }
+        });
     }
 }
